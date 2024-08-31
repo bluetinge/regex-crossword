2ef91c7acea9a3826725654f23a7721775399cbc
bluetinge <68611961+bluetinge@users.noreply.github.com>
2024-08-31 01:03:47 -0400

    Update crossword.js
    Really trying to figure out how to get this working on mobile

--- crossword.js
+++ crossword.js
@@ -261,10 +261,7 @@ function ruleDisplay(axis, idx) {
   var open = '';
   var close = '';
   if (use_editor) {
-    open = '<span 
-              ondblclick="editRule(\'' + axis + '\', ' + idx + ');"
-              onmousedown="editRule(\'' + axis + '\', ' + idx + ');"
-        >';
+    open = '<span onmousedown="editRule(\'' + axis + '\', ' + idx + ');">';
     close = '</span>';
   }
   return open + board_data[axis][idx] + close;
